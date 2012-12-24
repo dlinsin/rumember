57767149ee2a80a100c4689b795c84ddcdbab444
David Linsin <dlinsin@gmail.com>
2012-12-24 11:04:26 +0100

    Write passed arguments to output in order to enable Alfred to display a Notification

--- lib/rumember.rb
+++ lib/rumember.rb
@@ -41,6 +41,7 @@ class Rumember
       puts "Logged in as #{account.username}"
     else
       account.smart_add(argv.join(' '))
+      puts "ADDED: " + argv.join(' ')
     end
   rescue Error
     $stderr.puts "#$!"
